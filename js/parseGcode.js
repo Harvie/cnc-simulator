4ebcaeb3ac68f135a3897ddb4c241755342444a7
Tomas Mudrunka <tomas.mudrunka@gmail.com>
2024-09-17 00:43:11 +0200

    Preliminary version of circular interpolation (G2 G3)

--- js/parseGcode.js
+++ js/parseGcode.js
@@ -16,9 +16,11 @@
 // along with jscut.  If not, see <http://www.gnu.org/licenses/>.
 
 var jscut = jscut || {};
-
-jscut.parseGcode = function (options, gcode) {
+jscut.parseGcode = function (options, gcode, arcPrecision) {
     "use strict";
+
+    arcPrecision = arcPrecision || 0.1;
+
     var startTime = Date.now();
     if (options.profile)
         console.log("parseGcode...");
@@ -60,11 +62,8 @@ jscut.parseGcode = function (options, gcode) {
             else
                 ++i;
         }
-        if (g == 0 || g == 1 || g==2 || g==3) {
-		if(g == 2 || g == 3) {
-			console.log("G2/G3 not implemented, replacing ARC with single line!");
-		}
 
+        if (g == 0 || g == 1) { //Straight line
             if (!isNaN(x)) {
                 if (isNaN(lastX))
                     for (var j = 0; j < path.length; j += stride)
@@ -89,11 +88,63 @@ jscut.parseGcode = function (options, gcode) {
                         path[j] = f;
                 lastF = f;
             }
-            path.push(lastX);
-            path.push(lastY);
-            path.push(lastZ);
-            path.push(lastF);
-        }
+	            path.push(lastX);
+        	    path.push(lastY);
+            	    path.push(lastZ);
+            	    path.push(lastF);
+
+
+      } else if (g == 2 || g == 3) { //Arc or Circle
+
+    		//console.log("LAST: X" + lastX + " Y" + lastY + " Z" + lastZ);
+    		//console.log("G" + g + " X" + x + " Y" + y + " Z" + z + " I" + I + " J" + J + " K" + K + " R" + R);
+
+		if(!isNaN(R) || !isNaN(K)) console.log("G02/G03 K and R are not supported. Only I and J can be used for now.")
+		if(isNaN(I) && !isNaN(J)) I=0;
+		if(isNaN(J) && !isNaN(I)) J=0;
+
+    		var clockwise = (g == 2);
+    		var centerX = lastX + I;
+    		var centerY = lastY + J;
+    		//var centerZ = lastZ + K;
+    		var radius = Math.sqrt(Math.pow(lastX - centerX, 2) + Math.pow(lastY - centerY, 2));
+
+    		var startAngle = Math.atan2(lastY - centerY, lastX - centerX);
+    		var endAngle = Math.atan2(y - centerY, x - centerX);
+
+    	// Correct the angle difference based on the direction of the arc
+    	let angleDiff = endAngle - startAngle;
+    	if (clockwise) {
+        	if (angleDiff >= 0) angleDiff -= 2 * Math.PI;  // Ensure the angle is negative for clockwise
+    	} else {
+        	if (angleDiff <= 0) angleDiff += 2 * Math.PI;  // Ensure the angle is positive for counterclockwise
+    	}
+
+    	var arcLength = Math.abs(angleDiff * radius);  // Arc length based on angle
+    	var steps = Math.ceil(arcLength / arcPrecision);  // Number of steps based on precision
+
+    	for (let i = 1; i <= steps; i++) {
+        	var currentAngle = startAngle + (angleDiff * i) / steps;
+        	var interpX = centerX + radius * Math.cos(currentAngle);  // Interpolated X
+        	var interpY = centerY + radius * Math.sin(currentAngle);  // Interpolated Y
+		var interpZ = lastZ + ((z-lastZ)*i)/steps;
+		if(isNaN(interpZ)) interpZ=lastZ;
+
+        	// Add interpolated point to the path
+		//console.log("INTER: X" + interpX + " Y" + interpY + " Z" + interpZ);
+        	path.push(interpX);
+        	path.push(interpY);
+        	path.push(interpZ);  //FIXME: Assuming Z remains constant for the arc
+        	path.push(lastF); //Ignore F
+    	}
+
+    	// Update last position to final arc endpoint
+    	lastX = x;
+    	lastY = y;
+    	lastZ = z;
+	}
+
+
         while (i < gcode.length && gcode[i] != '\r' && gcode[i] != '\n')
             ++i;
         while (i < gcode.length && (gcode[i] == '\r' || gcode[i] == '\n'))
